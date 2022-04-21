d754b580636b9b6a16da7ca0d004f4138f7197fc
Abdul Malik Jabel Yusif <malikbanks01@gmail.com>
2022-04-21 03:27:36 +0000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let newHeader = document.createElement("h1");
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = "Malik is the champion";
+
+console.log(newHeader);
